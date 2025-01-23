833b7a2bb46fe0215db3b681529a0f56751ac679
Lucas Viana <lucasviana0604@gmail.com>
2025-01-23 19:23:38 -0300

    chore(database): update database uri import"

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,6 +3,6 @@ import { MongooseModule } from "@nestjs/mongoose";
 export const MongoDBModule = MongooseModule.forRootAsync({
     inject: [],
     useFactory: () => ({
-        uri: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.w0hvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+        uri: process.env.MONGO_URI,
     }),
 })
